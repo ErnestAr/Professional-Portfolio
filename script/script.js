9631441eecf0c5316952e7391600014d809e8bcd
Ernest Arutiunian <arutiunian.ernest@gmail.com>
2021-06-05 19:38:19 -0400

    final changes made

--- script/script.js
+++ script/script.js
@@ -1,30 +1,36 @@
-var pjct1 =$(".pjct-1")
-var pjct2 =$(".pjct-2")
-var pjct3 =$(".pjct-3")
 
 
 
-pjct1.on("click", function (event) {
-    var onoff = pjct1.attr("id");
+$(".opn").on("click", function (event) {
+    console.log(this)
+    var onoff = $(this).attr("name");
     if (onoff==="on") {
-        pjct1.children().eq(1).height('300px');
-        pjct1.children().eq(0).css("height", "300px");
-        pjct1.attr("id", "off");
-        $(".hide").css("display", "none");
-        $(".rmore").css("display", "inline");
-        $(".pjct-1").children().eq(0).children().eq(5).removeClass("tlsshow");
-        $(".pjct-1").children().eq(0).children().eq(5).addClass("tools");
+        $(this).children().eq(1).attr("style",'height: 300px; opacity: 0.7;');
+        $(this).children().eq(0).height( "300px");
+        $(this).attr("name", "off");
+        var hide = $(this).children().eq(0).children().eq(3)
+        if (hide!==null) {
+            $(this).children().eq(0).children().eq(3).css("display", "none");
+        }
+        $(this).children().eq(0).children().eq(2).css("display", "inline");
+        $(this).children().eq(0).children().eq(4).removeClass("tlsshow");
+        $(this).children().eq(0).children().eq(4).addClass("tools");
     } else if (onoff==="off") {
-        pjct1.children().eq(1).height('500px');
-        pjct1.children().eq(0).css("height", "500px");
-        pjct1.attr("id", "on");
-        $('.hide').css("display", "inline");
-        $(".rmore").css("display", "none");
-        $(".pjct-1").children().eq(0).children().eq(5).removeClass("tools");
-        $(".pjct-1").children().eq(0).children().eq(5).addClass("tlsshow");
+        $(this).children().eq(1).attr("style",'height: 500px; opacity: 1.0;');
+        $(this).children().eq(0).height( "fit-content");
+        $(this).attr("name", "on");
+        var hide = $(this).children().eq(0).children().eq(3)
+        if (hide!==null) {
+            $(this).children().eq(0).children().eq(3).css("display", "inline");
+        }
+        $(this).children().eq(0).children().eq(2).css("display", "none");
+        $(this).children().eq(0).children().eq(4).removeClass("tools");
+        $(this).children().eq(0).children().eq(4).addClass("tlsshow");
     }
 
 })
+
+
 
 
 
