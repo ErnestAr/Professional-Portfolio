b43dfa92d206bad18dc513f855d6285dc3e71336
ErnestAr <arutiunian.ernest@gmail.com>
2021-07-24 22:24:47 -0400

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,29 +6,6 @@ import  { useState } from 'react'
 
 
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
-  }
-
   return (
     <section id="contact" className="relative">
       <footer className="py-6 bg-coolGray-100 text-coolGray-900">
